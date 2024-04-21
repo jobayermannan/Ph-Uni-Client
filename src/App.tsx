1c14f70cd1045b7c1a646bf806d4f5834bd576d0
jobayermannan <jobayermannan777@gmail.com>
2024-04-21 07:31:56 +0600

    setting up protect route in the full app

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import MainLayout from "./components/layout/MainLayout"
+import ProtectedRoute from "./components/layout/ProtectedRoute"
 
 
 
@@ -8,7 +9,7 @@ function App() {
 
   return (
   <div>
-<MainLayout></MainLayout>
+     <ProtectedRoute><MainLayout></MainLayout></ProtectedRoute>
   </div>
   )
 }
