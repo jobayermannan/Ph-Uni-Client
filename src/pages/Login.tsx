abd87e861ef2d17f6902ed0d239f0209d742599e
jobayermannan <jobayermannan777@gmail.com>
2024-04-21 07:30:38 +0600

    redux persist and protected route setup done

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,19 +1,44 @@
 import { Button } from 'antd'
 
 import { useForm } from 'react-hook-form'
+import { useLoginMutation } from '../redux/features/auth/authApi'
+import { useAppDispatch } from '../redux/hook'
+import { setUser } from '../redux/features/auth/authSlice'
+import { verifyToken } from '../utils/verifyToken'
+
 
 const Login = () => {
 
-	 const {register,handleSubmit} =useForm()
+	const dispatch= useAppDispatch ()
 
-	 const onSubmit =(data) =>{
+	 const {register,handleSubmit} =useForm({
+		defaultValues:{
+			userId:"A-0001",
+		   password:"admin123" 
+		}
+	 })
+
+	 const [login,{error}] =useLoginMutation()
+
+
+
+	 const onSubmit =async(data:{ userId: string, password: string }) =>{
      console.log( data)
+	  const userInfo= {
+		id:data.userId,
+      password: data.password,
+
+	  }
+	  const res = await login(userInfo ).unwrap()
+	  const  user =verifyToken(res.data.accessToken)
+	  console.log(user )
+	  dispatch (setUser	({user:user,token: res.data.accessToken}))
 	 }
   return (
 	 <form onSubmit={handleSubmit(onSubmit)}>
 		 <div>
 		 <label htmlFor='id'> ID:</label>
-		  <input type='text' id='id' {...register('id')}></input>
+		  <input type='text' id='id' {...register('userId')}></input>
 		 </div>
 		 <div>
 		 <label htmlFor='password'> Pa:</label>
